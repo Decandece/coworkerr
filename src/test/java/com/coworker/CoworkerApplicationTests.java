19a100937f68aec378c52a943ba97357e90d5cc4
Cristian <cmbarrera2242@gmail.com>
2025-09-02 19:14:39 -0500

    first commit

---
+++ src/test/java/com/coworker/CoworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoworkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
